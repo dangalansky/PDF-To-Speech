cb98a48d0a20c99ef3a4db6872f839365d824493
dan_hates_the_future <97214702+dangalansky@users.noreply.github.com>
2022-10-24 12:37:53 -0600

    Add files via upload

--- say_it.py
+++ say_it.py
@@ -3,7 +3,7 @@ from tkinter import *
 from pygame import mixer
 
 
-# functions
+# FUNCTIONS
 def convert_text():
     speech = gTTS(text=text_type.get(), lang='en', slow=False)
     speech.save('voice.mp3')
@@ -21,14 +21,14 @@ window.geometry('600x300')
 window.title("SAY IT!")
 Canvas(height=600, width=600, bg='#000000', highlightthickness=0).place(x=0, y=0)
 
-# Entries
+# ENTRIES
 Label(window, text="SAY IT!", font=('Draconian Typewriter', 30), bg='#000000', fg="#FFFFFF").pack(pady=20)
 Label(window, text="enter yr text hurrrr:", font=('Draconian Typewriter', 20), bg='#000000', fg="#FFFFFF").pack(pady=30)
 
 text_type = StringVar()
 Entry(window, textvariable=text_type, width=70, font=('Arial', 12)).pack()
 
-# buttons
+# BUTTONS
 Button(window, text="Convert to Speech", highlightbackground='#000000', command=convert_text).pack(pady=10)
 Button(window, text="Say IT!", highlightbackground='#000000', command=play).pack(pady=10)
 
